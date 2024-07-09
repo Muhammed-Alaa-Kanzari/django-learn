6f76abc7aad9eae805a0ac95a88e0ea9d05c37af
Muhammed-Alaa-Kanzari <muhammed.alaa.kanzari@gmail.com>
2024-07-09 17:51:21 +0100

    first commit

---
+++ doc.md
@@ -0,0 +1,4 @@
+# Returning an object using the get() method
+# all method to retrieve a set of objects
+# retrieving objects by filtering
+# filtering by field lookups
---
+++ reviews/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Contributor, Publisher
+
+
+# Register your models here.
+
+admin.site.register(Contributor)
+admin.site.register(Publisher)
